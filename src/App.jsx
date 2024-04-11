e72ad20ac55e01719a57565af9c3f761a1d3b68c
rajugupta0 <raju200115@gmail.com>
2024-04-11 14:10:32 +0530

    Starter Template For frontEnd

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className="text-3xl font-bold bg-zinc-100 underline">
+    Hello world!
+  </h1>
+    </>
+  )
+}
+
+export default App
